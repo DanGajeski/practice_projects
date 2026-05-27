6df3c49cdaec6b6414cf7705a374dcdf5b3ebac0
Dan <dan@Dans-MacBook-Air.local>
2026-05-26 23:07:31 -0700

    Unit Circle with sin/cosine mapping

--- unit_circle.py
+++ unit_circle.py
@@ -1,22 +1,89 @@
 import argparse
-import tkinter as tk
 import math
+import tkinter as tk
 
 parser = argparse.ArgumentParser(description="A Test for argparse")
 parser.add_argument("--theta", action="store")
 
 args = parser.parse_args()
-#if args.theta:
+# if args.theta:
 #    print(f"Finding theta angle of {args.theta}")
 
 circle_origin_x = 250
 circle_origin_y = 250
 radius = 100
 main_line_length = radius
-#used for cos line
+# used for cos line
 main_line_endpoint_x: int = 0
-#used for sin line
+# used for sin line
 main_line_endpoint_y: int = 0
+
+
+def draw_title() -> None:
+    canvas.create_text(
+        circle_origin_x,
+        50,
+        text="The Unit Circle\nPlotting Sine and Cosine",
+        font=("Arial", 25, "bold"),
+        justify="center",
+        fill="black",
+        anchor="center",
+    )
+
+
+def draw_theta_text(
+    main_line_midpoint_x,
+    main_line_midpoint_y,
+    main_line_endpoint_x,
+    main_line_endpoint_y,
+) -> None:
+    if main_line_endpoint_y - circle_origin_y <= 0:  # check if y endpoint is positive
+        if (
+            main_line_endpoint_x - circle_origin_x >= 0
+        ):  # check if x endpoint is positive
+            canvas.create_text(
+                main_line_midpoint_x - 7,
+                main_line_midpoint_y - 7,
+                text=f"Theta : {str(args.theta)}",
+                angle=int(args.theta),
+                fill="green",
+                anchor="center",
+                # canvas.create_text(200, 200, text="Rotated Text", angle=45, font=("Arial", 20))
+            )
+        else:  # check if x endpoint is negative
+            canvas.create_text(
+                main_line_midpoint_x + 7,
+                main_line_midpoint_y - 7,
+                text=f"Theta : {str(args.theta)}",
+                angle=int(args.theta) + 180,
+                fill="green",
+                anchor="center",
+                # canvas.create_text(200, 200, text="Rotated Text", angle=45, font=("Arial", 20))
+            )
+    else:  # check if y endpoint is negative
+        if (
+            main_line_endpoint_x - circle_origin_x >= 0
+        ):  # check if x endpont is positive
+            canvas.create_text(
+                main_line_midpoint_x - 7,
+                main_line_midpoint_y + 7,
+                text=f"Theta : {str(args.theta)}",
+                angle=int(args.theta),
+                fill="green",
+                anchor="center",
+                # canvas.create_text(200, 200, text="Rotated Text", angle=45, font=("Arial", 20))
+            )
+        else:  # check if x endpoint is negative
+            canvas.create_text(
+                main_line_midpoint_x + 7,
+                main_line_midpoint_y + 7,
+                text=f"Theta : {str(args.theta)}",
+                angle=int(args.theta) + 180,
+                fill="green",
+                anchor="center",
+                # canvas.create_text(200, 200, text="Rotated Text", angle=45, font=("Arial", 20))
+            )
+
 
 def draw_unit_circle() -> None:
     canvas.create_oval(
@@ -25,9 +92,10 @@ def draw_unit_circle() -> None:
         circle_origin_x + radius,
         circle_origin_y + radius,
         fill="white",
-        outline="red",
-        width=3,
-        )
+        outline="black",
+        width=2,
+    )
+
 
 def draw_x_line() -> None:
     canvas.create_line(
@@ -38,6 +106,8 @@ def draw_x_line() -> None:
         fill="black",
         width="1",
     )
+
+
 def draw_y_line() -> None:
     canvas.create_line(
         circle_origin_x,
@@ -48,20 +118,16 @@ def draw_y_line() -> None:
         width="1",
     )
 
+
 def draw_main_line() -> tuple:
-    #convert degrees to radians as math functions use radians
+    # convert degrees to radians as math functions use radians
     angle_in_radians = math.radians(int(args.theta))
-    #calculate end points with origin at center of unit circle
-    #using '-' for y-calculation if you waant 0deg=right, 90deg=up
+    # calculate end points with origin at center of unit circle
+    # using '-' for y-calculation if you waant 0deg=right, 90deg=up
     x2 = circle_origin_x + main_line_length * math.cos(angle_in_radians)
     y2 = circle_origin_y - main_line_length * math.sin(angle_in_radians)
 
-    #setting global variables
-    #global variables need to be changed
-    main_line_x = x2
-    main_line_y = y2
-
-    #set to return value instead?
+    # set to return value instead?
     canvas.create_line(
         circle_origin_x,
         circle_origin_y,
@@ -71,19 +137,108 @@ def draw_main_line() -> tuple:
         width="3",
     )
 
-    return x2, y2
+    end_point = (x2, y2)
+    origin_point = (circle_origin_x, circle_origin_y)
 
-def draw_cosine_line(main_line_x, main_line_y) -> None:
+    mid_point_x = 0
+    mid_point_y = 0
+
+    def get_midpoint():
+        mid_point_x = (origin_point[0] + end_point[0]) / 2
+        mid_point_y = (origin_point[1] + end_point[1]) / 2
+
+        return mid_point_x, mid_point_y
+
+    mid_point_x, mid_point_y = get_midpoint()
+
+    return x2, y2, mid_point_x, mid_point_y
+
+
+def draw_cosine_line_and_text(main_line_x, main_line_y) -> None:
+    end_point = (main_line_x, circle_origin_y)
+    origin_point = (circle_origin_x, circle_origin_y)
+
+    mid_point_x = 0
+    mid_point_y = 0
+
+    def get_midpoint():
+        mid_point_x = (origin_point[0] + end_point[0]) / 2
+        mid_point_y = (origin_point[1] + end_point[1]) / 2
+
+        return mid_point_x, mid_point_y
+
+    mid_point_x, mid_point_y = get_midpoint()
+
     canvas.create_line(
         circle_origin_x,
         circle_origin_y,
         main_line_x,
         circle_origin_y,
         fill="blue",
-        width="3"
+        width="3",
     )
+    if (
+        main_line_y - circle_origin_y <= 0
+    ):  # if endpoint of line is above y=0 on the unit circle
+        if (
+            main_line_x - circle_origin_x >= 0
+        ):  # if endpoint of line is right of x=0 on the unit circle
+            canvas.create_text(
+                # circle_origin_x + 5,
+                # circle_origin_y + 5,
+                mid_point_x,
+                mid_point_y + 10,
+                text="cosine of theta",
+                fill="blue",
+                anchor="center",
+            )
+            print(main_line_x - circle_origin_x)
+        else:  # if endpoint of line is left of x=0 on the unit circle
+            canvas.create_text(
+                mid_point_x,
+                mid_point_y + 10,
+                text="cosine of theta",
+                fill="blue",
+                anchor="center",
+            )
+            print(main_line_x - circle_origin_x)
+    else:  # if endpoint of line is below y=0 on the unit circle
+        if (
+            main_line_x - circle_origin_x >= 0
+        ):  # if endpont of line is right of x=0 on the unit circle
+            canvas.create_text(
+                mid_point_x,
+                mid_point_y - 10,
+                text="cosine of theta",
+                fill="blue",
+                anchor="center",
+            )
+            print(main_line_x - circle_origin_x)
+        else:  # if endpoint of line is left of x=0 on the unit circle
+            canvas.create_text(
+                mid_point_x,
+                mid_point_y - 10,
+                text="cosine of theta",
+                fill="blue",
+                anchor="center",
+            )
+            print(main_line_x - circle_origin_x)
 
-def draw_sine_line(main_line_x, main_line_y) -> None:
+
+def draw_sine_line_and_text(main_line_x, main_line_y) -> None:
+    end_point = (main_line_x, main_line_y)
+    origin_point = (main_line_x, circle_origin_y)
+    mid_point_x = 0
+    mid_point_y = 0
+
+    def get_midpoint():
+        mid_point_x = (origin_point[0] + end_point[0]) / 2
+        mid_point_y = (origin_point[1] + end_point[1]) / 2
+
+        return mid_point_x, mid_point_y
+
+    mid_point_x, mid_point_y = get_midpoint()
+
     canvas.create_line(
         main_line_x,
         main_line_y,
@@ -92,24 +247,52 @@ def draw_sine_line(main_line_x, main_line_y) -> None:
         fill="red",
         width="3",
     )
+    # print sin of theta on right side
+    if main_line_x - circle_origin_x >= 0:
+        canvas.create_text(
+            mid_point_x + 5,
+            mid_point_y,
+            text="sine of theta",
+            fill="red",
+            anchor="w",
+        )
+        print(main_line_x - circle_origin_x)
+    # print sin of theta on left side
+    else:
+        canvas.create_text(
+            mid_point_x - 5,
+            mid_point_y,
+            text="sine of theta",
+            fill="red",
+            anchor="e",
+        )
+        print(main_line_x - circle_origin_x)
 
 
 root = tk.Tk()
-canvas = tk.Canvas(root, width=500, height=500, borderwidth=0, highlightthickness=0, bg="black")
+canvas = tk.Canvas(
+    root, width=500, height=500, borderwidth=0, highlightthickness=0, bg="white"
+)
 canvas.pack(fill="both", expand=True)
 
-
-
-
-
-
-
+draw_title()
 draw_unit_circle()
 draw_x_line()
 draw_y_line()
-main_line_endpoint_x, main_line_endpoint_y = draw_main_line()
-draw_cosine_line(main_line_endpoint_x, main_line_endpoint_y)
-draw_sine_line(main_line_endpoint_x, main_line_endpoint_y)
-print("TEST TEXT FOR GITHUB UPLOAD")
+(
+    main_line_endpoint_x,
+    main_line_endpoint_y,
+    main_line_midpoint_x,
+    main_line_midpoint_y,
+) = draw_main_line()
+draw_theta_text(
+    main_line_midpoint_x,
+    main_line_midpoint_y,
+    main_line_endpoint_x,
+    main_line_endpoint_y,
+)
+draw_cosine_line_and_text(main_line_endpoint_x, main_line_endpoint_y)
+draw_sine_line_and_text(main_line_endpoint_x, main_line_endpoint_y)
+# print("TEST TEXT FOR GITHUB UPLOAD")
 
 root.mainloop()
